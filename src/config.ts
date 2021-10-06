f86bac99e8f0fb4a42ce348a3cee7f4ee8452055
last-arg <last_arg@tutanota.com>
2021-10-06 20:40:11 +0300

    fix partial html files locations

--- src/config.ts
+++ src/config.ts
@@ -6,18 +6,18 @@ export const urlRoot = "/twitch-pages"
 export const mainContent: Record<string, UrlResolve> = {
   "top-games": {
     url: urlRoot,
-    html: "/public/partials/top-games.html",
+    html: "/partials/top-games.html",
   },
   "category": {
     url: urlRoot + "/directory/game/:name",
-    html: "/public/partials/category.html",
+    html: "/partials/category.html",
   },
   "user-videos": {
     url: urlRoot + "/:user/videos",
-    html: "/public/partials/user-videos.html",
+    html: "/partials/user-videos.html",
   },
   "not-found": {
     url: urlRoot + "/not-found",
-    html: "/public/partials/not-found.html",
+    html: "/partials/not-found.html",
   }
 }
