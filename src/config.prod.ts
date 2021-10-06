4418e9e21682dbe902cfefb223be667587ee064b
last-arg <last_arg@tutanota.com>
2021-10-06 21:08:16 +0300

    fix prod partials path

--- src/config.prod.ts
+++ src/config.prod.ts
@@ -6,18 +6,18 @@ export const urlRoot = "/twitch-pages"
 export const mainContent: Record<string, UrlResolve> = {
   "top-games": {
     url: urlRoot,
-    html: "/partials/top-games.html",
+    html: urlRoot + "/partials/top-games.html",
   },
   "category": {
     url: urlRoot + "/directory/game/:name",
-    html: "/partials/category.html",
+    html: urlRoot + "/partials/category.html",
   },
   "user-videos": {
     url: urlRoot + "/:user/videos",
-    html: "/partials/user-videos.html",
+    html: urlRoot + "/partials/user-videos.html",
   },
   "not-found": {
     url: urlRoot + "/not-found",
-    html: "/partials/not-found.html",
+    html: urlRoot + "/partials/not-found.html",
   }
 }
