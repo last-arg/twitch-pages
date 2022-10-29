342f6b53e7dceec37dfd1d14bdba897929672549
last-arg <last_arg@tutanota.com>
2022-10-29 20:18:36 +0300

    change eleventy output directory to '_site'

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,13 +4,12 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
     components: "src/_includes/components/*.webc"
 	});
-  eleventyConfig.setWatchJavaScriptDependencies(true);
   eleventyConfig.addWatchTarget("src/main.js");
   eleventyConfig.addPassthroughCopy("public");
 	return {
   	dir: {
     	input: "src",
-    	output: "tmp",
+    	output: "_site",
   	}
 	};
 };
