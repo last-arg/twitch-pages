bc7da8fc6ec5095987b16cf7a1ca0426c2debc5e
last-arg <last_arg@tutanota.com>
2022-11-03 21:09:55 +0200

    clean code

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,6 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
     components: "src/_includes/components/*.webc"
 	});
-  // eleventyConfig.setWatchThrottleWaitTime(15);
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addPassthroughCopy("public");
   eleventyConfig.addPassthroughCopy("favicon.svg");
