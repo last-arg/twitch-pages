e89efebd9caa68d241e17dbe25196cc9f0ba9014
last-arg <last_arg@tutanota.com>
2022-11-13 10:31:45 +0200

    clean unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -5,13 +5,9 @@ import * as fs from 'fs/promises';
 
 const config = defineConfig({
   rules: [
-    // ['fill-current', { fill: 'currentColor' }],
     [/^stack\-?(\d*)(\w*)$/, ruleStack, {layer: "component"}],
     [/^stack\-?(\w*)$/, ruleStackFluid, {layer: "component"}],
     [/^l-grid-?(.*)$/, ruleLayoutGrid, {layer: "component"}],
-    // [/^sidebar\-button$/, ruleSidebarButton, {layer: "component"}],
-    // [/^sidebar\-wrapper$/, ruleSidebarWrapper, {layer: "component"}],
-    // [/^filter\-checkbox\-btn$/, ruleFilterCheckboxBtn, {layer: "component"}],
   ],
   shortcutsLayer: "component",
   preflights: [
@@ -30,77 +26,6 @@ function fileContent(filename: string) {
     const srcStyleCss = await fs.readFile(filename)
     return srcStyleCss.toString();
   }
-}
-
-
-async function ruleSidebarButton([], ctx) {
-  const generator = ctx.generator;
-  let result = ""
-  const matched = generator.matchVariants("")
-  const default_rules = [
-    "block", "h-full", "px-3", "pt-1.5", "pb-0.5", "border-b-5",
-    "text-gray-200", "bg-gray-900", "border-gray-900",
-    "hover:text-gray-50", "hover:text-gray-50", "hover:border-gray-500",
-    "focus:text-gray-50", "focus:text-gray-50", "focus:border-gray-500",
-  ]
-
-  const selectors = await generator.stringifyShortcuts(matched, ctx, default_rules)
-  const escapedSelector = escapeSelector(ctx.rawSelector)
-  for (const [,selector, css_body] of selectors) {
-    result += selector.replace(".", "." + escapedSelector)
-    result += `{${css_body}}\n`
-  }
-
-  const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["border-violet-700"])
-  result += `.${escapedSelector}[aria-expanded="true"] {${css_body}}`
-  return result
-}
-
-async function ruleSidebarWrapper([], ctx) {
-  const generator = ctx.generator;
-  let result = ""
-  const matched = generator.matchVariants("")
-  const classSelector = "." + escapeSelector(ctx.rawSelector)
-  {
-    const default_rules = ["h-screen", "pt-11", "pb-2", "absolute", "top-0", "left-full", "max-w-20rem", "w-full", "transform", "-z-10"]
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, default_rules)
-    result += `${classSelector}{${css_body}transition: visibility 150ms, opacity 150ms, transform 150ms;}\n`
-  }
-  {
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["translate-x-0", "opacity-0", "invisible"])
-    result += `[aria-expanded="false"] ~ ${classSelector}{${css_body}}\n`
-  }
-  {
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["opacity-100", "visible"])
-    result += `[aria-expanded="true"] ~ ${classSelector}{${css_body};--un-translate-x:-100%;}\n`
-  }
-  return result
-}
-
-async function ruleFilterCheckboxBtn([], ctx) {
-  const generator = ctx.generator;
-  let result = ""
-  const matched = generator.matchVariants("")
-  const classSelector = "." + escapeSelector(ctx.rawSelector)
-  {
-    const default_rules = ["p-1", "border-r-2", "border-truegray-50"]
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, default_rules)
-    result += `${classSelector}{${css_body}}\n`
-  }
-  {
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["bg-lime-300"])
-    result += `${classSelector}.archive.checked > span{${css_body}}\n`
-  }
-  {
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["bg-sky-300"])
-    result += `${classSelector}.upload.checked > span{${css_body}}\n`
-  }
-  {
-    const [[,,css_body]] = await generator.stringifyShortcuts(matched, ctx, ["bg-amber-300"])
-    result += `${classSelector}.highlight.checked > span{${css_body}}\n`
-  }
-  
-  return result
 }
 
 function ruleStack([selector, nr, unit]: RegExpMatchArray) {
@@ -139,7 +64,7 @@ ${classSelector} > * + * { margin-top: var(${var_name}, 1em); }
     `
 }
 
-async function ruleLayoutGrid([selector, min_width]: RegExpMatchArray, ctx) {
+async function ruleLayoutGrid([selector, min_width]: RegExpMatchArray, ctx: any) {
   const generator = ctx.generator;
   const classSelector = "." + escapeSelector(selector)
   if (min_width === '') {
