f0b4bbb3e56c6f40913ea4fc04839736d3360891
last-arg <last_arg@tutanota.com>
2022-11-18 15:52:30 +0200

    config

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,11 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
-    components: "src/_includes/components/*.webc"
+    components: "src/_includes/components/*.webc",
 	});
+  // eleventyConfig.addWatchTarget("src/_includes/components/");
   eleventyConfig.setUseGitIgnore(false);
-	eleventyConfig.ignores.add("src/css");
+	eleventyConfig.ignores.add("src/css/");
   eleventyConfig.addPassthroughCopy("public");
   eleventyConfig.addPassthroughCopy("favicon.svg");
 	return {
