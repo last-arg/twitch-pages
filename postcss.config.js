cb8d794c007446a1267986630c498224fcf79193
last-arg <last_arg@tutanota.com>
2022-11-25 22:30:09 +0200

    postcss config for production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
+const plugins = [
+  require('postcss-import')(),
+];
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push(require("cssnano")());
+  plugins.push(require("postcss-prune-var")());
+}
+
 module.exports = {
-  plugins: [
-    require('postcss-import')
-  ]
+  plugins: plugins
 }
