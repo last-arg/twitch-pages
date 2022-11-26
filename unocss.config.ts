e3fd0494a0b63e9226cdbb6695993b9d2e659bfb
last-arg <last_arg@tutanota.com>
2022-11-26 18:17:18 +0200

    setup production build

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, escapeSelector } from "unocss";
 // TODO: add l-gap for l-grid
 const config = defineConfig({
   cli: {entry: { patterns: ["src/*.webc", "src/**/*.webc", "src/**/**/*.webc"], outFile: "src/css/utilities_generated.css"} },
+  preflights: undefined,
   rules: [
     [/^stack\-?(\d*)(.*)$/, ruleStack, {layer: "component"}],
     [/^l-grid-?(.*)$/, ruleLayoutGrid, {layer: "component"}],
