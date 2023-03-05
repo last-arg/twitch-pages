5e87dd4c35140966889cbc2b3dfaae3ada2fa610
last-arg <last_arg@tutanota.com>
2023-03-05 14:39:25 +0200

    generate css from eleventy pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,41 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const htmlMinifier = require ('html-minifier')
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
-    components: "./src/_includes/components/**/*.webc",
+		components: "./src/_includes/components/**/*.webc",
 	});
-  eleventyConfig.setUseGitIgnore(false);
+	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.ignores.add("src/css/");
-  eleventyConfig.addPassthroughCopy("public");
-  eleventyConfig.addPassthroughCopy("src/sw.js");
-  eleventyConfig.addPassthroughCopy("favicon.svg");
+	eleventyConfig.addPassthroughCopy("public");
+	eleventyConfig.addPassthroughCopy("src/sw.js");
+	eleventyConfig.addPassthroughCopy("favicon.svg");
+
+	  eleventyConfig.addTransform("component-css", async function(content) {
+		if (this.page.outputPath === "src/css/components.css") {
+			const index = content.indexOf("---");
+			return content.slice(0, index);
+		}
+
+	    return content; // no change done.
+	  });
+
+  eleventyConfig.addTransform ('html-minifier', content => {
+    if (process.env.NODE_ENV === 'production') {
+      return htmlMinifier.minify (content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+    }
+    return content
+  })
+
 	return {
 		// domdiff: false,
-  	dir: {
-    	input: "src",
-    	output: "_site",
-  	}
+		dir: {
+			input: "src",
+			output: "_site",
+		}
 	};
 };
