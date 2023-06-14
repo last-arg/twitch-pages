68c780e9dbf772be78380835f0b3a10ae0d9ceca
last-arg <last_arg@tutanota.com>
2023-06-14 12:51:10 +0300

    move css config from package.json to eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,25 +1,25 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const htmlMinifier = require ('html-minifier')
 const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+const purgeCssPlugin = require("eleventy-plugin-purgecss");
 
 module.exports = function(eleventyConfig) {
+	const is_prod = process.env.ELEVENTY_ENV === "prod";
+
+	if (process.env.ELEVENTY_ENV === "prod") {
+		eleventyConfig.addPlugin(purgeCssPlugin)
+	}
+	eleventyConfig.addPlugin(lightningCSS, { minify: is_prod });
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: ["./src/_includes/components/**/*.webc"],
 	});
 
-	// eleventyConfig.addPlugin(lightningCSS)
-	// eleventyConfig.addPlugin(lightningCSS, {
-	// 	visitors: [
-	// 		(d) => {
-	// 			console.log("visit")
-	// 		}
-	// 	]
-	// });
-
 	eleventyConfig.setUseGitIgnore(false);
-	eleventyConfig.ignores.add("src/css/");
+	eleventyConfig.watchIgnores.add("src/css/_components.css");
+	eleventyConfig.watchIgnores.add("src/css/_utilities_generated.css");
+	eleventyConfig.ignores.add("src/css/_*.css");
 	eleventyConfig.addPassthroughCopy("public");
-	eleventyConfig.addPassthroughCopy("src/sw.js");
+	// eleventyConfig.addPassthroughCopy("src/sw.js");
 	eleventyConfig.addPassthroughCopy("favicon.svg");
 
     eleventyConfig.addTransform ('html-minifier', content => {
