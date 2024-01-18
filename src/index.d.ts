ff638d04c5de2e5fe02b51520e7d3944689b17e1
last_arg <last_arg@tutanota.com>
2024-01-18 16:38:28 +0200

    remove typescript dep

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,10 +2,3 @@ declare var htmx: any;
 declare var Idiomorph: {
     morph: (old_node: any, new_content: any, config?: any) => void;
 };
-
-declare global {
-    interface Window { 
-        filterItems: typeof pageFilter;
-        resetFilter: typeof resetFilter;
-    }
-}
