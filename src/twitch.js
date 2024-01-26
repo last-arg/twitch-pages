c62ae9a2dc3e3f530275ad860081588a9880e35f
last_arg <last_arg@tutanota.com>
2024-01-26 09:31:10 +0200

    fix logic

--- src/twitch.js
+++ src/twitch.js
@@ -36,8 +36,8 @@ export class Twitch {
 
   async fetchToken() {
     let token = this.twitch_token
-    const now_seconds = Date.now() / 1000;
-    if (!token || token.expires_date > now_seconds) {
+    const now_seconds = Math.floor(Date.now() / 1000);
+    if (!token || token.expires_date < now_seconds) {
       this.is_fetching_token = true;
       const r = await fetch("/api/twitch-api/?new-token");
       if (r.status !== 200) {
