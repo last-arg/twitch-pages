1c9b1fcc8fa54c307b12a6cf9c6f2f2483acc8c3
last_arg <last_arg@tutanota.com>
2024-02-08 19:25:09 +0200

    fix js import, fix build

--- src/third-party.js
+++ src/third-party.js
@@ -1,4 +1,8 @@
-import * as htmx from "htmx.org";
+import * as htmx from "htmx.org/dist/htmx";
+globalThis.htmx = htmx;
+// @ts-ignore
 import { Idiomorph } from "../node_modules/idiomorph/dist/idiomorph.esm";
-window.htmx = htmx;
-window.Idiomorph = Idiomorph;
+globalThis.Idiomorph = Idiomorph;
+// @ts-ignore
+// Make sure this is loaded after making htmx global (window/globalThis)
+require("htmx.org/dist/ext/preload");
